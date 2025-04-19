9becadb1cd4bf6e1daa1e8df37a8dd07906aa068
dasunT22 <dasuntharinda2535@gmail.com>
2025-04-19 23:56:32 +0530

    add missing part of courier Profile

--- BookMart/app/views/courierProfile.view.php
+++ BookMart/app/views/courierProfile.view.php
@@ -38,6 +38,8 @@
                             
                         <?php if(!empty($courierD)): ?>
                             <?php foreach ($courierD as $courierDetail): ?>
+                                <?php if(!empty($courier)): ?>
+                                    <?php foreach ($courier as $courierD): ?>
 
                             <div class="form-group-row">
                                 <div class="form-group">
@@ -113,7 +115,7 @@
                             <div class="form-group-row">
                                 <div class="form-group">
                                     <label for="email-address">Email Address:</label>
-                                    <input type="text" id="email-address"  name="email" placeholder=" " required>
+                                    <input type="text" id="email-address"  name="email" placeholder="<?= $courierD->email ?> " required>
                                 </div>
                                 
                             </div>
@@ -233,6 +235,8 @@
                             <button class="next-button" type="submit" >Change & Save</button>
                             <br>
                             <br>
+                            <?php endforeach; ?>
+                            <?php endif; ?>
                             <?php endforeach; ?>
                                 <?php else: ?>
                                     <p>No</p>
